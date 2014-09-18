5cf33e4182032ef3899ed3ffa0be83e2840227e3
Johan Larsson <johan_gavle1@hotmail.com>
2014-09-18 18:45:27 +0200

    -> laptop

---
+++ Wpf.FlipView.Tests/FlipViewUiTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Wpf.FlipView.Tests
+{
+    using System.Windows.Automation.Peers;
+
+    using NUnit.Framework;
+    /// <summary>
+    /// http://miketwo.blogspot.se/2007/03/unit-testing-wpf-controls-with.html
+    /// </summary>
+    public class FlipViewUiTests
+    {
+        [Test, RequiresSTA]
+        public void TestNameTest()
+        {
+            var window = new MainWindow();
+            var windowPeer = new WindowAutomationPeer(window);
+            window.Show();
+            List<AutomationPeer> automationPeers = windowPeer.GetChildren();
+            var flipViewPeer = automationPeers.OfType<WPF.FlipView.FlipView>().Single();
+        }
+    }
+}
