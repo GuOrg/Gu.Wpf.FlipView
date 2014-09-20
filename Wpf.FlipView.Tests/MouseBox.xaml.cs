2839ead26306e063869f1172b635963f09c322f1
Johan Larsson <johan_gavle1@hotmail.com>
2014-09-20 11:55:15 +0200

    Fixed copy-paste dumb in mouse

--- Wpf.FlipView.Tests/MouseBox.xaml.cs
+++ Wpf.FlipView.Tests/MouseBox.xaml.cs
@@ -4,14 +4,37 @@ using System.Windows.Controls;
 
 namespace Wpf.FlipView.Tests
 {
+    using System.Windows.Input;
+
     /// <summary>
     /// Interaction logic for MouseBox.xaml
     /// </summary>
     public partial class MouseBox : EventBox
     {
+        private bool _gestureStarted;
         public MouseBox()
         {
             InitializeComponent();
+        }
+
+        protected override void OnStarted(object sender, InputEventArgs e)
+        {
+            _gestureStarted = true;
+            base.OnStarted(sender, e);
+        }
+
+        protected override void OnInput(object sender, InputEventArgs e)
+        {
+            if (_gestureStarted)
+            {
+                base.OnInput(sender, e);
+            }
+        }
+
+        protected override void OnEnded(object sender, InputEventArgs e)
+        {
+            _gestureStarted = false;
+            base.OnEnded(sender, e);
         }
     }
 }
