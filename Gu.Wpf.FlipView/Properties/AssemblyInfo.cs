c5e3bc3b40e431354bb17be34076ea83edacc177
Johan Larsson <johan_gavle1@hotmail.com>
2015-02-24 17:23:36 +0100

    XmlnsPrefix

--- Gu.Wpf.FlipView/Properties/AssemblyInfo.cs
+++ Gu.Wpf.FlipView/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -51,7 +52,9 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: InternalsVisibleTo("Gu.Wpf.FlipView.Tests", AllInternalsVisible = true)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
+[assembly: XmlnsDefinition("http://gu.se/FlipView", "Gu.FlipView")]
+[assembly: XmlnsPrefix("http://gu.se/FlipView", "flipView")]
