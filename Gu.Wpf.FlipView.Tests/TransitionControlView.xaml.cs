3f763948fc796bf31c78f25d5e95914a7d22e1c5
Johan Larsson <johan_gavle1@hotmail.com>
2015-05-05 09:42:13 +0200

    Starting refactoring to TransitionControl

---
+++ Gu.Wpf.FlipView.Tests/TransitionControlView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Gu.Wpf.FlipView.Tests
+{
+    /// <summary>
+    /// Interaction logic for TransitionControlView.xaml
+    /// </summary>
+    public partial class TransitionControlView : UserControl
+    {
+        private Button _button;
+
+        public TransitionControlView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (_button != null)
+            {
+                _button.Content = TransitionControl.Content;
+            }
+            var button = (Button)e.Source;
+            TransitionControl.Content = button.Content;
+            _button = button;
+        }
+    }
+}
---
+++ Gu.Wpf.FlipView/TransitionControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Gu.Wpf.FlipView
+{
+    public class TransitionControl : ContentControl
+    {
+        static TransitionControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TransitionControl), new FrameworkPropertyMetadata(typeof(TransitionControl)));
+        }
+
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            base.OnContentChanged(oldContent, newContent);
+        }
+    }
+}
