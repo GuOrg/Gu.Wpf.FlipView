27958fe97f4a49fcd8a21917f2ca6ade94e2c75c
JohanLarsson <johan_gavle1@hotmail.com>
2017-06-20 11:37:38 +0200

    Add view with default animations for transition control.

---
+++ Gu.Wpf.FlipView.Demo/ControlDemos/TransitionControlStyledView.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Gu.Wpf.FlipView.Demo.ControlDemos
+{
+    using System.Collections.Generic;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    public partial class TransitionControlStyledView : UserControl
+    {
+        public TransitionControlStyledView()
+        {
+            this.InitializeComponent();
+            this.Items = new[]
+                        {
+                            new TransitionItem(Brushes.Blue, "1"),
+                            new TransitionItem(Brushes.Red, "2"),
+                            new TransitionItem(Brushes.Yellow, "3"),
+                        };
+        }
+
+        public IReadOnlyCollection<TransitionItem> Items { get; }
+    }
+}
--- Gu.Wpf.FlipView.Demo/ControlDemos/TransitionControlView.xaml.cs
+++ Gu.Wpf.FlipView.Demo/ControlDemos/TransitionControlView.xaml.cs
@@ -4,20 +4,17 @@
     using System.Windows.Controls;
     using System.Windows.Media;
 
-    /// <summary>
-    /// Interaction logic for TransitionControlView.xaml
-    /// </summary>
     public partial class TransitionControlView : UserControl
     {
         public TransitionControlView()
         {
             this.InitializeComponent();
             this.Items = new[]
-                        {
-                            new TransitionItem(Brushes.Blue, "1"),
-                            new TransitionItem(Brushes.Red, "2"),
-                            new TransitionItem(Brushes.Yellow, "3"),
-                        };
+                         {
+                             new TransitionItem(Brushes.Blue, "1"),
+                             new TransitionItem(Brushes.Red, "2"),
+                             new TransitionItem(Brushes.Yellow, "3"),
+                         };
         }
 
         public IReadOnlyCollection<TransitionItem> Items { get; }
