cecb35e16c3698787d4c64a573bbbe90d84130f7
JohanLarsson <johan_gavle1@hotmail.com>
2017-06-20 11:53:08 +0200

    Add UI-test project and test that just checks that all views load.

---
+++ Gu.Wpf.FlipView.UiTests/MainWindowTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Gu.Wpf.FlipView.UiTests
+{
+    using FlaUI.Core;
+    using FlaUI.Core.Definitions;
+    using FlaUI.UIA3;
+    using NUnit.Framework;
+
+    public class MainWindowTests
+    {
+        [Test]
+        public void ClickAllTabs()
+        {
+            // Just a smoke test so that everything builds.
+            using (var app = Application.Launch(Info.ProcessStartInfo))
+            {
+                using (var automation = new UIA3Automation())
+                {
+                    var window = app.GetMainWindow(automation);
+                    var tab = window.FindFirstDescendant(x => x.ByControlType(ControlType.Tab));
+                    foreach (var element in tab.FindAllChildren(x => x.ByControlType(ControlType.TabItem)))
+                    {
+                        var tabItem = element.AsTabItem();
+                        tabItem.Click();
+                    }
+                }
+            }
+        }
+    }
+}
