f6a55a07ec6442ca9fbd5d01a9d4b6f4d0da1111
Johan Larsson <johan_gavle1@hotmail.com>
2017-06-20 22:23:55 +0200

    Docs.

--- Gu.Wpf.FlipView/Gestures/IGestureInterpreter.cs
+++ Gu.Wpf.FlipView/Gestures/IGestureInterpreter.cs
@@ -11,12 +11,14 @@
         /// <summary>
         /// Check if <paramref name="points"/> is a gesture.
         /// </summary>
+        /// <param name="points">The points registered by the tracker.</param>
         bool TryGetGesture(IReadOnlyList<GesturePoint> points, out GestureEventArgs gestureEventArgs);
 
         /// <summary>
-        /// Check if <paramref name="eventArgs"/> can be interpreted as a gesture.
+        /// Check if <paramref name="executed"/> can be interpreted as a gesture.
         /// Some hardware turns swipes into navigation commands.
         /// </summary>
-        bool TryGetGesture(ExecutedRoutedEventArgs eventArgs, out GestureEventArgs gestureEventArgs);
+        /// <param name="executed">The event args for the command that executed.</param>
+        bool TryGetGesture(ExecutedRoutedEventArgs executed, out GestureEventArgs gestureEventArgs);
     }
 }
