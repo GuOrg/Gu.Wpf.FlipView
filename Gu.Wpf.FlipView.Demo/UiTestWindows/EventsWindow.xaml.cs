02b3044f0a7ca0bd08631c119fcddf98de08db2b
Johan Larsson <johan_gavle1@hotmail.com>
2018-08-23 20:38:33 +0200

    EventsWindow

---
+++ Gu.Wpf.FlipView.Demo/UiTestWindows/EventsWindow.xaml.cs
@@ -0,0 +1,12 @@
+namespace Gu.Wpf.FlipView.Demo.UiTestWindows
+{
+    using System.Windows;
+
+    public partial class EventsWindow : Window
+    {
+        public EventsWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
