d5de83e1f7e8057076d3b4227768c9ee3f930c4a
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-23 10:34:36 +0200

    Refactor swipe test.

--- Gu.Wpf.FlipView.UiTests/MouseGesturePanelWindowTests.cs
+++ Gu.Wpf.FlipView.UiTests/MouseGesturePanelWindowTests.cs
@@ -15,20 +15,15 @@ namespace Gu.Wpf.FlipView.UiTests
                 var window = app.MainWindow;
                 var gesturePanel = window.FindGroupBox("Gesture panel");
                 var listBox = window.FindListBox("Gestures");
-                Mouse.MoveTo(gesturePanel.Bounds.Center());
                 var swipes = new List<string>();
                 CollectionAssert.IsEmpty(listBox.Items);
 
-                Mouse.Down(MouseButton.Left);
-                Mouse.MoveBy(100, 0);
-                Mouse.Up(MouseButton.Left);
-                swipes.Add("SwipeRight Delta: (100, 0) Velocity: 0,3");
+                Mouse.DragHorizontally(MouseButton.Left, gesturePanel.Bounds.Center(), 15);
+                swipes.Add("SwipeRight Delta: (15, 0) Velocity: ∞");
                 CollectionAssert.AreEqual(swipes, listBox.Items.Select(x => x.Text));
 
-                Mouse.Down(MouseButton.Left);
-                Mouse.MoveBy(-100, 0);
-                Mouse.Up(MouseButton.Left);
-                swipes.Add("SwipeLeft Delta: (-100, 0) Velocity: 0,3");
+                Mouse.DragHorizontally(MouseButton.Left, gesturePanel.Bounds.Center(), -15);
+                swipes.Add("SwipeLeft Delta: (-15, 0) Velocity: ∞");
                 CollectionAssert.AreEqual(swipes, listBox.Items.Select(x => x.Text));
             }
         }
