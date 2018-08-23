16147cb7e326d1c60132a4007c51db7cd159700a
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-23 10:56:05 +0200

    More tests for GesturePanel.

--- Gu.Wpf.FlipView.UiTests/MouseGesturePanelWindowTests.cs
+++ Gu.Wpf.FlipView.UiTests/MouseGesturePanelWindowTests.cs
@@ -1,7 +1,9 @@
 namespace Gu.Wpf.FlipView.UiTests
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Windows;
     using Gu.Wpf.UiAutomation;
     using NUnit.Framework;
 
@@ -19,12 +21,62 @@ namespace Gu.Wpf.FlipView.UiTests
                 CollectionAssert.IsEmpty(listBox.Items);
 
                 Mouse.DragHorizontally(MouseButton.Left, gesturePanel.Bounds.Center(), 15);
+                window.WaitUntilResponsive();
                 swipes.Add("SwipeRight Delta: (15, 0) Velocity: ∞");
                 CollectionAssert.AreEqual(swipes, listBox.Items.Select(x => x.Text));
 
                 Mouse.DragHorizontally(MouseButton.Left, gesturePanel.Bounds.Center(), -15);
+                window.WaitUntilResponsive();
                 swipes.Add("SwipeLeft Delta: (-15, 0) Velocity: ∞");
                 CollectionAssert.AreEqual(swipes, listBox.Items.Select(x => x.Text));
+            }
+        }
+
+        [Test]
+        public void NoSwipesWhenTooShort()
+        {
+            using (var app = Application.Launch("Gu.Wpf.FlipView.Demo.exe", "MouseGesturePanelWindow"))
+            {
+                var window = app.MainWindow;
+                var gesturePanel = window.FindGroupBox("Gesture panel");
+                var listBox = window.FindListBox("Gestures");
+                CollectionAssert.IsEmpty(listBox.Items);
+
+                Mouse.DragHorizontally(MouseButton.Left, gesturePanel.Bounds.Center(), 14);
+                window.WaitUntilResponsive();
+                CollectionAssert.IsEmpty(listBox.Items);
+
+                Mouse.DragHorizontally(MouseButton.Left, gesturePanel.Bounds.Center(), -14);
+                window.WaitUntilResponsive();
+                CollectionAssert.IsEmpty(listBox.Items);
+            }
+        }
+
+        [Test]
+        public void NoSwipesWhenTooSlow()
+        {
+            using (var app = Application.Launch("Gu.Wpf.FlipView.Demo.exe", "MouseGesturePanelWindow"))
+            {
+                var window = app.MainWindow;
+                var gesturePanel = window.FindGroupBox("Gesture panel");
+                var listBox = window.FindListBox("Gestures");
+                CollectionAssert.IsEmpty(listBox.Items);
+
+                Mouse.Position = gesturePanel.Bounds.Center();
+                Mouse.Down(MouseButton.Left);
+                Wait.For(TimeSpan.FromMilliseconds(200));
+                Mouse.Position += new Vector(15, 0);
+                Mouse.Up(MouseButton.Left);
+                window.WaitUntilResponsive();
+                CollectionAssert.IsEmpty(listBox.Items);
+
+                Mouse.Position = gesturePanel.Bounds.Center();
+                Mouse.Down(MouseButton.Left);
+                Wait.For(TimeSpan.FromMilliseconds(200));
+                Mouse.Position += new Vector(-15, 0);
+                Mouse.Up(MouseButton.Left);
+                window.WaitUntilResponsive();
+                CollectionAssert.IsEmpty(listBox.Items);
             }
         }
     }
