0eda8fb936794fec5c6c4c03a5839b812af90be6
JohanLarsson <johan_gavle1@hotmail.com>
2020-04-27 11:07:17 +0200

    Sign.
    Fix #14

--- Gu.Wpf.FlipView/AssemblyAttributes.cs
+++ Gu.Wpf.FlipView/AssemblyAttributes.cs
@@ -2,7 +2,7 @@ using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Markup;
 
-[assembly: InternalsVisibleTo("Gu.Wpf.FlipView.Tests", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Gu.Wpf.FlipView.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ed009756dd50ca13f2a484be8db257502eb0814c28710bfe4c7de6a7719ddc96bc7823e07e03cd6c4c45287721a76256466fd5079fc28612bb93dfdfcd26b75ed39ce16f8239c40c64a248f8e6d94e7d435fb0a2b97f6b77712f4bbf14ada327a71c80915529b2797104fa36f6a6cb3ccd1a1ad12b5cc9dd056f46b60c43a5b8", AllInternalsVisible = true)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 [assembly: XmlnsDefinition("https://github.com/JohanLarsson/Gu.Wpf.FlipView", "Gu.Wpf.FlipView")]
