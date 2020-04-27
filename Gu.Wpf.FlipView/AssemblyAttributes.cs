f4f4bfab7b78b8b64be266142b330773f0d7fc57
JohanLarsson <johan_gavle1@hotmail.com>
2020-04-27 09:51:12 +0200

    Use nuget.
    Paket is still better but nuget is in the vs box.

--- Gu.Wpf.FlipView/AssemblyAttributes.cs
+++ Gu.Wpf.FlipView/AssemblyAttributes.cs
@@ -1,25 +1,10 @@
-using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
-[assembly: AssemblyTitle("Gu.Wpf.FlipView")]
-[assembly: AssemblyDescription("A flip view that works on Win7. Handles mouse & touch.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Johan Larsson")]
-[assembly: AssemblyProduct("Gu.Wpf.FlipView")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("Gu.Wpf.FlipView.Tests", AllInternalsVisible = true)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
-[assembly: InternalsVisibleTo("Gu.Wpf.FlipView.Tests", AllInternalsVisible = true)]
-[assembly: NeutralResourcesLanguageAttribute("en")]
 [assembly: XmlnsDefinition("https://github.com/JohanLarsson/Gu.Wpf.FlipView", "Gu.Wpf.FlipView")]
 [assembly: XmlnsDefinition("https://github.com/JohanLarsson/Gu.Wpf.FlipView", "Gu.Wpf.FlipView.AttachedProperties")]
 [assembly: XmlnsDefinition("https://github.com/JohanLarsson/Gu.Wpf.FlipView", "Gu.Wpf.FlipView.Gestures")]
