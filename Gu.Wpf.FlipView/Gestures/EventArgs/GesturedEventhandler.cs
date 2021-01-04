6c2b88dd3148f8a8e9585bab269c4bdbec309d11
JohanLarsson <johan_gavle1@hotmail.com>
2021-01-04 16:26:12 +0100

    Fix warnings.

--- Gu.Wpf.FlipView/Gestures/EventArgs/GesturedEventhandler.cs
+++ Gu.Wpf.FlipView/Gestures/EventArgs/GesturedEventhandler.cs
@@ -6,6 +6,8 @@ namespace Gu.Wpf.FlipView.Gestures
     /// <param name="sender">The sender.</param>
     /// <param name="e">The <see cref="GestureEventArgs"/>.</param>
 #pragma warning disable CA1711 // Identifiers should not have incorrect suffix
+#pragma warning disable CA1003 // Use generic event handler instances
     public delegate void GesturedEventHandler(object sender, GesturedEventArgs e);
+#pragma warning restore CA1003 // Use generic event handler instances
 #pragma warning restore CA1711 // Identifiers should not have incorrect suffix
 }
