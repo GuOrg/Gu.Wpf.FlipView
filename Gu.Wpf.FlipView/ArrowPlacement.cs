94c4b5ed320989c0d84669f22bd43ce1c1dbc740
Stefano Tenuta <stapps@outlook.it>
2021-02-06 13:28:56 +0100

    Added Top/Bottom placements for Arrows.

--- Gu.Wpf.FlipView/ArrowPlacement.cs
+++ Gu.Wpf.FlipView/ArrowPlacement.cs
@@ -1,4 +1,4 @@
-﻿namespace Gu.Wpf.FlipView
+namespace Gu.Wpf.FlipView
 {
     /// <summary>
     /// For indicating how navigation arrows should be placed.
@@ -11,8 +11,28 @@
         Inside,
 
         /// <summary>
+        /// Inside the image but on top row.
+        /// </summary>
+        InsideTop,
+
+        /// <summary>
+        /// Inside the image but on bottom row.
+        /// </summary>
+        InsideBottom,
+
+        /// <summary>
         /// Outside the image.
         /// </summary>
         Outside,
+
+        /// <summary>
+        /// Outside the image but on top row.
+        /// </summary>
+        OutsideTop,
+
+        /// <summary>
+        /// Outside the image but on bottom row.
+        /// </summary>
+        OutsideBottom,
     }
 }
